370c3851e44fd91421729337c05e2690ddc4d026
Alex <paltus3@yandex.ru>
2023-09-06 14:06:49 +0300

    flake8-nofront_tests0

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
