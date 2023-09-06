85e6b6fdc3f476e19566149019fbcd8ee4d773fe
Alex <paltus3@yandex.ru>
2023-09-06 20:58:16 +0300

    testing

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -4,7 +4,7 @@ from recipes.models import Ingredient, Recipe
 
 class RecipeFilter(FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
+    is_favorited = filters.CharFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='get_is_in_shopping_cart'
     )
