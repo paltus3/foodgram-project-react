334428979784f85dc01f387867e6040790b7b01f
Alex <paltus3@yandex.ru>
2023-09-07 13:22:30 +0300

    fix_cart

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -4,8 +4,8 @@ from recipes.models import Ingredient, Recipe
 
 class RecipeFilter(FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_favorited = filters.CharFilter(method='get_is_favorited')
+    is_in_shopping_cart = filters.CharFilter(
         method='get_is_in_shopping_cart'
     )
 
